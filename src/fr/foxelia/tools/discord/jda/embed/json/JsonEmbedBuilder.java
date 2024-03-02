e38a9cb0874c6a8e2e48d785adea006bbfef452c
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-03-02 21:21:23 +0100

    Add Embed for JDA

---
+++ src/fr/foxelia/tools/discord/jda/embed/json/JsonEmbedBuilder.java
@@ -0,0 +1,139 @@
+package fr.foxelia.tools.discord.jda.embed.json;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.awt.*;
+import java.sql.Timestamp;
+
+/**
+ * This class is used to create an discord embed from a JSON file
+ * <br>It use the GSON library to parse the JSON file and give a formatted embed for the JavaDiscordAPI
+ * <br>License : CC BY-SA 4.0
+ * <br>Author : Zarinoow, Foxelia
+ * @version 1.0
+ */
+public class JsonEmbedBuilder {
+
+    private final JsonObject jsonEmbed;
+
+    /**
+     * This constructor is used to create an embed from a JSON formatted {@link java.lang.String}
+     * @param jsonContent The JSON content of the embed
+     * @throws JsonSyntaxException If the JSON content is not valid
+     */
+    public JsonEmbedBuilder(String jsonContent) throws JsonSyntaxException {
+        this(JsonParser.parseString(jsonContent).getAsJsonObject());
+    }
+
+    /**
+     * This constructor is used to create an embed from a {@link com.google.gson.JsonObject}
+     * @param jsonEmbed The JSON object of the embed
+     */
+    public JsonEmbedBuilder(JsonObject jsonEmbed) {
+        this.jsonEmbed = jsonEmbed;
+    }
+
+    /**
+     * Call this method to build the embed
+     * @return The built embed
+     * @see MessageEmbed
+     */
+    public MessageEmbed build() {
+        /*
+         * Constants needed for to build the embed
+         */
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        // Set the author
+        if(jsonEmbed.has("author")) {
+            JsonObject author = jsonEmbed.get("author").getAsJsonObject();
+            embedBuilder.setAuthor(editString(author.get("name").getAsString()),
+                    editURL(author.get("url").getAsString()),
+                    editURL(author.get("icon_url").getAsString()));
+        }
+
+        // Set the title
+        if(jsonEmbed.has("title")) {
+            embedBuilder.setTitle(editString(jsonEmbed.get("title").getAsString()));
+        }
+
+        // Set the description
+        if(jsonEmbed.has("description")) {
+            embedBuilder.setDescription(editString(jsonEmbed.get("description").getAsString()));
+        }
+
+        // Set the url
+        if(jsonEmbed.has("url")) {
+            embedBuilder.setUrl(editURL(jsonEmbed.get("url").getAsString()));
+        }
+
+        // Set the image
+        if(jsonEmbed.has("image")) {
+            JsonObject image = jsonEmbed.get("image").getAsJsonObject();
+            embedBuilder.setImage(editURL(image.get("url").getAsString()));
+        }
+
+        // Set the thumbnail
+        if(jsonEmbed.has("thumbnail")) {
+            JsonObject thumbnail = jsonEmbed.get("thumbnail").getAsJsonObject();
+            embedBuilder.setThumbnail(editURL(thumbnail.get("url").getAsString()));
+        }
+
+        // Set the color
+        if(jsonEmbed.has("color")) {
+            embedBuilder.setColor(Color.decode(jsonEmbed.get("color").getAsString()));
+        }
+
+        // Set the footer
+        if(jsonEmbed.has("footer")) {
+            JsonObject footer = jsonEmbed.get("footer").getAsJsonObject();
+            embedBuilder.setFooter(editString(footer.get("text").getAsString()),
+                    editURL(footer.get("icon_url").getAsString()));
+        }
+
+        // Set the timestamp
+        if(jsonEmbed.has("timestamp")) {
+            embedBuilder.setTimestamp(new Timestamp(jsonEmbed.get("timestamp").getAsLong()).toInstant());
+        }
+
+        // Set the fields
+        if(jsonEmbed.has("fields")) {
+            for(int i = 0; i < jsonEmbed.get("fields").getAsJsonArray().size(); i++) {
+                JsonObject field = jsonEmbed.get("fields").getAsJsonArray().get(i).getAsJsonObject();
+                boolean inline = false;
+                if(field.has("inline")) inline = field.get("inline").getAsBoolean();
+                embedBuilder.addField(editString(field.get("name").getAsString()),
+                        editString(field.get("value").getAsString()),
+                        inline);
+            }
+        }
+
+        return embedBuilder.build();
+    }
+
+    /**
+     * This method is called for each string field in the JSON file
+     * You can edit the String before the line is added to the embed
+     * @apiNote To edit the method, you can override it in a subclass
+     * @param string The String to edit
+     * @return The edited String
+     */
+    public String editString(String string) {
+        return string;
+    }
+
+    /**
+     * This method is called for each URL field in the JSON file
+     * You can edit the URL before the line is added to the embed
+     * @apiNote To edit the method, you can override it in a subclass
+     * @param url The URL to edit
+     * @return The edited URL
+     */
+    public String editURL(String url) {
+        return url;
+    }
+
+}
---
+++ src/fr/foxelia/tools/discord/jda/embed/json/README.md
@@ -0,0 +1,67 @@
+# JSON Embed Builder for JDA
+
+[![License](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)
+[![Discord](https://img.shields.io/discord/341897164642975756?color=blue&label=Discord)](https://discord.foxelia.fr/)
+[![GitHub](https://img.shields.io/github/stars/FoxeliaFR/RandomJavaTools?style=social)](https://github.com/FoxeliaFR/RandomJavaTools)
+
+Embeds are a great way to make your bot look professional. They can be used to display information in a nice and organized way. This class allows you to create embeds easily and quickly. You can use it to create embeds for your bot using JDA.
+
+## Why I created this class ?
+
+Because I'm a big fan of configuration files. I like to have a configuration file for everything. So I wanted to create a class that allows me to create embeds using a json file. This way, I can easily create embeds and modify them without having to modify the code.
+
+## License
+[<img src="https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg">](https://creativecommons.org/licenses/by-sa/4.0/)<br>
+<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg" alt="CC BY-SA 4.0" width="200" height="70"><br>
+This work is licensed under a
+[Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/).<br>
+Read the [LICENSE](LICENSE.md) file for license rights and limitations (CC BY-SA 4.0).
+
+## Am I allowed to use this class ?
+Sure, you can use this class in your plugins. You can also modify it and redistribute it. But you must respect the credits (do not remove them or change them to your own name) and the license (see section above).
+
+## Used Libraries
+
+Before using this class, you must add the following libraries to your project :
+
+| Library Name                                                         |
+|----------------------------------------------------------------------|
+| [JavaDiscordAPI](https://github.com/discord-jda/JDA)                 |
+| [Gson](https://mvnrepository.com/artifact/com.google.code.gson/gson) | 
+
+## How to use it ?
+You can use this class in your projects in two different ways :
+- Add the dependency to your project using Maven, Gradle, or any other dependency manager.
+- You can also import the class directly into your project.
+
+First, you need to create a new JSONEmbedBuilder object. Then you can use the following methods to create your embed :
+
+```java
+String jsonEmbed = "{\"title\": \"My Title\", \"description\": \"My Description\", \"color\": \"#ff0000\"}";
+
+JsonEmbedBuilder builder = new JsonEmbedBuilder(jsonEmbed);
+```
+
+To build the embed, simply use the build() method :
+
+```java
+MessageEmbed embed = builder.build();
+```
+
+You can also build it in one line :
+
+```java
+MessageEmbed embed = new JsonEmbedBuilder("{\"title\": \"My Title\", \"description\": \"My Description\", \"color\": \"#ff0000\"}").build();
+```
+
+To build the embed with the good fields, you can use the following tools created by [cubedhuang](https://github.com/cubedhuang) :
+- https://embed.dan.onl/
+
+If you need to read the embed from a file, refer to [JSONFileReader](src/fr/foxelia/tools/java/files/reader/).
+
+## Support
+If you have any questions, you can contact us on our discord server : [https://discord.foxelia.fr/](https://discord.foxelia.fr/)
+
+## Credit
+
+The JsonEmbedBuilder class was created by [Zarinoow](https://github.com/Zarinoow/) of the [Foxelia](https://foxelia.fr/) team.
